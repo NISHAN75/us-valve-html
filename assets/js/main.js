bdd58676559c4856f9cf45430b31a4262be44970
nishan das <nishandas880@gmail.com>
2026-07-09 18:53:32 +0600

    section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,124 @@
             });
         });
 
+         // Magnific popup
+        $(document).on('click', '.trigger-popup', function (e) {
+            e.preventDefault();
+            $.magnificPopup.open({
+                items: {
+                    src: $(this).attr('href')
+                },
+                type: 'iframe',
+                iframe: {
+                    markup: '<div class="mfp-iframe-scaler">' +
+                        '<div class="mfp-close"></div>' +
+                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen allow="autoplay *; fullscreen *"></iframe>' +
+                        '</div>',
+                    patterns: {
+                        youtube: {
+                            index: 'youtube.com/',
+                            id: function (url) {
+                                var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+                                if (!m || !m[1]) return null;
+                                return m[1];
+                            },
+                            src: '//www.youtube.com/embed/%id%?autoplay=1&iframe=true'
+                        },
+                        vimeo: {
+                            index: 'vimeo.com/',
+                            id: function (url) {
+                                var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+                                if (!m || !m[5]) return null;
+                                return m[5];
+                            },
+                            src: '//player.vimeo.com/video/%id%?autoplay=1'
+                        }
+                    }
+                },
+                callbacks: {
+                    open: function () {
+                        var iframe = jQuery('.mfp-content iframe');
+                        var player = new Vimeo.Player(iframe);
+
+                        player.on('ended', function () {
+                            jQuery.magnificPopup.close();
+                        });
+                    },
+                    close: function () {
+                        let video = document.getElementById("placeholder-video");
+                        if (video) {
+                            video.play();
+                        }
+                    }
+                }
+            });
+        });
+
+        // gsap
+
+        gsap.registerPlugin(ScrollTrigger);
+
+        // function lineAnimation() {
+
+        //     ScrollTrigger.getAll().forEach(st => st.kill());
+
+        //     gsap.killTweensOf(".line-one, .line-two, .line-three");
+
+        //     gsap.set(".line-one, .line-two, .line-three", {
+        //         clearProps: "transform"
+        //     });
+
+        //     gsap.to(".line-one", {
+        //         xPercent: -7.5,
+        //         skewX: -26,
+        //         duration: 1.8,
+        //         ease: "expo.out",
+        //         scrollTrigger: {
+        //             trigger: ".line-wrapper",
+        //             start: "top 80%",
+        //             once: true
+        //         }
+        //     });
+
+        //     gsap.to(".line-two", {
+        //         xPercent: -18.9,
+        //         skewX: -26,
+        //         duration: 1.8,
+        //         ease: "expo.out",
+        //         scrollTrigger: {
+        //             trigger: ".line-wrapper",
+        //             start: "top 80%",
+        //             once: true
+        //         }
+        //     });
+
+        //     gsap.to(".line-three", {
+        //         xPercent: -31.65,
+        //         skewX: -26,
+        //         duration: 1.8,
+        //         ease: "expo.out",
+        //         scrollTrigger: {
+        //             trigger: ".line-wrapper",
+        //             start: "top 80%",
+        //             once: true
+        //         }
+        //     });
+        // }
+
+        // lineAnimation();
+
+        // let resizeTimer;
+
+        // window.addEventListener("resize", () => {
+        //     clearTimeout(resizeTimer);
+
+        //     resizeTimer = setTimeout(() => {
+        //         lineAnimation();
+        //         ScrollTrigger.refresh();
+        //     }, 200);
+        // });
+
+
 
         // OverlayScrollbars
         const {
